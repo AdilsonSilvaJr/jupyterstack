5dfa72f09463d412a55c6509e26c639fef16345a
Adilson Silva <adilsonjunioraj@hotmail.com>
2022-03-12 19:04:39 -0300

    Change docker to kubernetes for airflow - This change improve a new way to implement airflow and connect with git

--- airflow/build-docker-airflow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p dags logs plugins
-chmod 777 dags logs plugins
-
-docker-compose up airflow-init
-
--- airflow/dags/Ingest_data_diabetes/Ingest_data_diabetes.py
+++ airflow/dags/Ingest_data_diabetes/Ingest_data_diabetes.py
@@ -1,30 +1,3 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-
-"""
-### ETL DAG Tutorial Documentation
-This ETL DAG is compatible with Airflow 1.10.x (specifically tested with 1.10.12) and is referenced
-as part of the documentation that goes along with the Airflow Functional DAG tutorial located
-[here](https://airflow.apache.org/tutorial_decorated_flows.html)
-"""
-# [START tutorial]
-# [START import_module]
 import json
 from datetime import datetime
 from textwrap import dedent
@@ -38,8 +11,6 @@ from airflow.operators.python import PythonOperator
 # [END import_module]
 
 # [START default_args]
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
 default_args = {
     'owner': 'airflow',
 }
@@ -47,13 +18,13 @@ default_args = {
 
 # [START instantiate_dag]
 with DAG(
-    'tutorial_etl_dag',
+    'Ingest_data_diabetes',
     default_args=default_args,
-    description='ETL DAG tutorial',
+    description='ETL DAG data diabetes',
     schedule_interval=None,
     start_date=datetime(2021, 1, 1),
     catchup=False,
-    tags=['example'],
+    tags=['diabetes'],
 ) as dag:
     # [END instantiate_dag]
     # [START documentation]

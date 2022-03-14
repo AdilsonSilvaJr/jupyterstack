c67344a879205ed41eb4504f73ed88eaf1576d50
Adilson Silva <adilsonjunioraj@hotmail.com>
2022-03-13 21:01:30 -0300

    Airflow on kubernetes

---
+++ airflow/install-airflow.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+##################################################################################################
+############## Chart from: https://artifacthub.io/packages/helm/airflow-helm/airflow ##############
+##################################################################################################
+
+############## (Step 1) - Add this helm repository: ##############
+## add this helm repository & pull updates from it
+helm repo add airflow-stable https://airflow-helm.github.io/charts
+helm repo update
+
+############## (Step 2) - Install chart: ##############
+## set the release-name & namespace
+export AIRFLOW_NAME="airflow-cluster"
+export AIRFLOW_NAMESPACE="airflow-cluster"
+
+#create namespace
+kubectl create ns $AIRFLOW_NAMESPACE
+
+# install using helm 3
+helm install \
+  $AIRFLOW_NAME \
+  airflow-stable/airflow \
+  --namespace $AIRFLOW_NAMESPACE \
+  --version "8.X.X" \
+  --values ./values-airflow.yaml
+  
+############## (Step 3) - Locally expose the airflow webserver: ##############  
+## port-forward the airflow webserver
+kubectl port-forward svc/${AIRFLOW_NAME}-web 8080:8080 --namespace $AIRFLOW_NAMESPACE
+
+## open your browser to: http://localhost:8080 
+## default login: admin/admin
+
+############## Upgrade ##############
+## pull updates from the helm repository
+helm repo update
+
+## apply any new values // upgrade chart version to 8.X.X
+helm upgrade \
+  $AIRFLOW_NAME \
+  airflow-stable/airflow \
+  --namespace $AIRFLOW_NAMESPACE \
+  --version "8.X.X" \
+  --values ./custom-values.yaml
+  
+############## Uninstall ##############
+## uninstall the chart
+helm uninstall $AIRFLOW_NAME --namespace $AIRFLOW_NAMESPACE

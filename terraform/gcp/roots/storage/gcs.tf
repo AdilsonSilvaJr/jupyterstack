a72b3e87126af06b581995702408d78c7e1e1753
Adilson Silva <adilsonjunioraj@hotmail.com>
2022-03-17 13:43:37 -0300

    Add GCS "bucket-diabetes-ml"

--- terraform/gcp/roots/storage/gcs.tf
+++ terraform/gcp/roots/storage/gcs.tf
@@ -2,3 +2,8 @@ resource "google_storage_bucket" "bucket-state" {
   name = "bucket-diabetes"
   location = "US-CENTRAL1"
 }
+
+resource "google_storage_bucket" "bucket-diabetes-ml" {
+  name = "bucket-diabetes-ml"
+  location = "US-CENTRAL1"
+}

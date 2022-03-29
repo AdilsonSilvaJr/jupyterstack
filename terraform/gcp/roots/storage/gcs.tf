e0e7846cf6031dab62b4618073fc9f0a83e0b82b
Adilson Silva <adilsonjunioraj@hotmail.com>
2022-03-29 14:52:49 -0300

    Create bucket for analytics

--- terraform/gcp/roots/storage/gcs.tf
+++ terraform/gcp/roots/storage/gcs.tf
@@ -7,3 +7,8 @@ resource "google_storage_bucket" "bucket-diabetes-ml" {
   name = "bucket-diabetes-ml"
   location = "US-CENTRAL1"
 }
+
+resource "google_storage_bucket" "bucket-diabetes-analytics" {
+  name = "bucket-diabetes-analytics"
+  location = "US-CENTRAL1"
+}

4b3984dedecc3d5672c4fdfcfe041737af167960
Adilson Silva <adilsonjunioraj@hotmail.com>
2022-04-03 17:49:40 -0300

    Join model and dashboard in app. Organize some archives.

---
+++ app/src/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import dashboard, model  # import your app modules here
+
+app = MultiApp()
+
+st.set_page_config(layout="wide")
+st.sidebar.markdown("""
+# Projeto  Predição de diabetes 
+## Time Jupyter 🚀
+
+Escrever alguma coisa aqui
+
+""")
+
+# Add all your application here
+app.add_app("Dashboard", dashboard.app)
+app.add_app("Model", model.app)
+# The main app
+app.run()
